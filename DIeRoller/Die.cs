e40d909abd99f9ed976e9ba84ab5d72b83809411
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-07-20 15:53:55 -0700

    Refactored roll method to take into account isHeld

--- DIeRoller/Die.cs
+++ DIeRoller/Die.cs
@@ -22,7 +22,7 @@ namespace DieRoller
         /// <summary>
         /// The current face up value of the die
         /// </summary>
-        public byte FaceValue { get; set; }
+        public byte FaceValue { get; private set; }
 
         /// <summary>
         /// True if the die is currently held
@@ -33,17 +33,20 @@ namespace DieRoller
 
         /// <summary>
         /// Rolls the die and sets the <see cref="FaceValue""/>
-        /// to the new number. Returns the new number
+        /// to the new number if the die is not currently
+        /// held. Returns the <see cref"FaceValue"/>
         /// </summary>
         /// <returns>Returns the new random number</returns>
         public byte Roll()
         {
-            // Generate random number
-            Random random = new Random();
-            byte newValue = (byte)random.Next(1, 7);
+            if (!IsHeld)
+            {
+                // Generate random number
+                Random random = new Random();
+                byte newValue = (byte)random.Next(1, 7);
 
-            FaceValue = newValue;
-
+                FaceValue = newValue;
+            }
             return FaceValue;
         }
     }
